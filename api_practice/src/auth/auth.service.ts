b51c67f421d90b7c63449f7201517112285ec5f7
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2024-04-17 15:57:24 +0100

    feat: implement authentication with passport and AuthGuards

--- api_practice/src/auth/auth.service.ts
+++ api_practice/src/auth/auth.service.ts
@@ -7,10 +7,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signIn(dto: AuthDto) {
     // find the user by email address
     const user = await this.prisma.user.findUnique({
@@ -25,9 +31,7 @@ export class AuthService {
     // verify that the user's password match
     const pwMatches = await argon.verify(user.hash, dto.password);
     if (!pwMatches) throw new UnauthorizedException('Credentials incorrect');
-    // incorrect password ? throw exception : send back the user
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
   }
 
   async signUp(dto: AuthDto) {
@@ -42,11 +46,7 @@ export class AuthService {
           hash,
         },
       });
-
-      delete user.hash;
-
-      // return the newly created user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         // a prisma error that violates a unique constraint
@@ -57,5 +57,22 @@ export class AuthService {
 
       throw error;
     }
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+    return {
+      access_token: token,
+    };
   }
 }
